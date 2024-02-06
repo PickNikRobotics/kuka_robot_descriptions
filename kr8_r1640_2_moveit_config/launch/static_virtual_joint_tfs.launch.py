b4820080efb80c2c5512e48380f259db4d186d0e
jbeck <jbeck@calvaryrobotics.com>
2024-02-06 07:21:50 -0500

    Fixed URDF for kr8_r1640_2
    Also made xacro usable without RSI inputs

---
+++ kr8_r1640_2_moveit_config/launch/static_virtual_joint_tfs.launch.py
@@ -0,0 +1,7 @@
+from moveit_configs_utils import MoveItConfigsBuilder
+from moveit_configs_utils.launches import generate_static_virtual_joint_tfs_launch
+
+
+def generate_launch_description():
+    moveit_config = MoveItConfigsBuilder("kr8_r1640-2", package_name="kr8_r1640_2_moveit_config").to_moveit_configs()
+    return generate_static_virtual_joint_tfs_launch(moveit_config)
