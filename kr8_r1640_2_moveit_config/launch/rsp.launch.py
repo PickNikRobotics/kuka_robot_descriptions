17df32a323d91b88c4e70972b4b7dd2bf14f05ce
Josh Beck <jbeck@calvaryrobotics.com>
2024-05-15 08:06:02 -0400

    Renamed a robot

--- kr8_r1640_2_moveit_config/launch/rsp.launch.py
+++ kr8_r1640_2_moveit_config/launch/rsp.launch.py
@@ -3,5 +3,5 @@ from moveit_configs_utils.launches import generate_rsp_launch
 
 
 def generate_launch_description():
-    moveit_config = MoveItConfigsBuilder("kr8_r1640-2", package_name="kr8_r1640_2_moveit_config").to_moveit_configs()
+    moveit_config = MoveItConfigsBuilder("kr8_r1640_2", package_name="kr8_r1640_2_moveit_config").to_moveit_configs()
     return generate_rsp_launch(moveit_config)
--- kr8_r1640_2_moveit_config/launch/setup_assistant.launch.py
+++ kr8_r1640_2_moveit_config/launch/setup_assistant.launch.py
@@ -3,5 +3,5 @@ from moveit_configs_utils.launches import generate_setup_assistant_launch
 
 
 def generate_launch_description():
-    moveit_config = MoveItConfigsBuilder("kr8_r1640-2", package_name="kr8_r1640_2_moveit_config").to_moveit_configs()
+    moveit_config = MoveItConfigsBuilder("kr8_r1640_2", package_name="kr8_r1640_2_moveit_config").to_moveit_configs()
     return generate_setup_assistant_launch(moveit_config)
